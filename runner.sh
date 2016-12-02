b14f97ea73ddda67205aebab70c9f1f0d640df75
Karl K <Karlk15@ru.is>
2016-12-02 19:15:00 +0000

    link to web added so webside should open after docker-compose runs up

--- runner.sh
+++ runner.sh
@@ -3,4 +3,5 @@ set -e               # skips to exit 0 if warning pops when cmd docker-compose u
   sleep 10           # gives server 10 seconds to start up and connect
   npm run migratedb  #
   node run.js        # node used to run js and run.js connects the ports
+  open http://52.214.95.76/
 exit 0               # if warning pops up this will be second step from set -e
