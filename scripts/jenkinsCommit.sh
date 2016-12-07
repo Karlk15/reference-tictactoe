b24c18b7351d5666dd25d01c1c05cdbbe4d1d817
Karl K <Karlk15@ru.is>
2016-12-07 16:33:19 +0000

    comments added

--- scripts/jenkinsCommit.sh
+++ scripts/jenkinsCommit.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-git clean -dfx
-git stash
-rm -rf node_modules
-npm install
+
+git clean -dfx        # Remove untracked files from the working tree...
+git stash             # Stash the changes in a dirty working directory away
+rm -rf node_modules   # Remove node_modules from root
+npm install           # installs npm
 cd client
-rm -rf node_modules
-npm install
-cd ..
-./pack.sh
+rm -rf node_modules   # Remove node_modules from client
+npm install           # installs npm should only be localy
+cd ..                 # steps out of client
+./pack.sh             # run pack script
+    
