ea59f724c0fbcd65546694108612190d3794caf0
Karl K <Karlk15@ru.is>
2016-12-08 13:32:03 +0000

    provision in progress, newFile : testExample, fix in jenkinsCommit

--- scripts/provisioning.sh
+++ scripts/provisioning.sh
@@ -9,3 +9,35 @@ set -e
   chmod +x /usr/local/bin/docker-compose
   docker-compose up
 exit 0
+
+
+#TODO ___________________________________________________________________________________________________________________________________________________________
+SECURITY_GROUP_ID=$(aws ec2 create-security-group --group-name MySecurityGroup --description "My security group")
+SECURITY_GROUP_NAME=$(KarlUser-key-pair-eu-west-1.pem)
+INSTANCE_ID=$(aws ec2 run-instances  --user-data file://ec2-instance-init.sh --image-id ami-9398d3e0 --security-group-ids ${SECURITY_GROUP_ID} --count 1 --instance-type t2.micro --key-name ${SECURITY_GROUP_NAME} --query 'Instances[0].InstanceId'  --output=text)
+
+echo ${INSTANCE_ID} > ./ec2_instance/instance-id.txt
+
+scp -o StrictHostKeyChecking=no -i "./ec2_instance/${SECURITY_GROUP_NAME}.pem" ./docker-compose.yaml ec2-user@${INSTANCE_PUBLIC_NAME}:~/docker-compose.yaml
+scp -o StrictHostKeyChecking=no -i "./ec2_instance/${SECURITY_GROUP_NAME}.pem" ./docker-compose-and-run.sh ec2-user@${INSTANCE_PUBLIC_NAME}:~/docker-compose-and-run.sh
+
+aws ec2 wait --region eu-west-1 instance-running --instance-ids ${INSTANCE_ID}
+
+yum -y update
+yum -y install docker
+pip install docker-compose
+pip install backports.ssl_match_hostname --upgrade
+service docker start
+usermod -a -G docker ec2-user
+touch ec2-init-done.markerfile
+
+status='unknown'
+while [ ! "${status}" == "ok" ]
+do
+   echo Checking status of host, currently ${status}
+   status=$(ssh -i "./ec2_instance/${SECURITY_GROUP_NAME}.pem"  -o StrictHostKeyChecking=no -o BatchMode=yes -o ConnectTimeout=5 ec2-user@${INSTANCE_PUBLIC_NAME} echo ok 2>&1)
+   sleep 2
+done
+
+
+ssh -o StrictHostKeyChecking=no -i "./ec2_instance/${SECURITY_GROUP_NAME}.pem" ec2-user@${INSTANCE_PUBLIC_NAME} "cat ~/docker-compose-and-run.sh"
