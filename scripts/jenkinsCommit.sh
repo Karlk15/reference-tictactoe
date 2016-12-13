06bbcd2cddf2a4938c0d9b247e6582604a744237
Karl K <Karlk15@ru.is>
2016-12-13 13:59:50 +0000

    Git_Commit for tag

--- scripts/jenkinsCommit.sh
+++ scripts/jenkinsCommit.sh
@@ -9,10 +9,10 @@ set -e
   npm install           # installs npm should only be localy
   cd ..
   cd scripts
-  #./cleanImages.sh
+  #./cleanImages.sh     # to clean images in ubuntu
   cd ..
   ./pack.sh             # run pack script
   scp -o StrictHostKeyChecking=no -i "~/workspace/TicTacToe_Deploy/KarlUser-key-pair-eu-west-1.pem" ./docker-compose.yaml ec2-user@52.208.180.163:~/docker-compose.yaml  #jenkins cp's docker-compose to aws server
   scp -o StrictHostKeyChecking=no -i "~/workspace/TicTacToe_Deploy/KarlUser-key-pair-eu-west-1.pem" ./scripts/docker-compose-and-run.sh ec2-user@52.208.180.163:~/docker-compose-and-run.sh  #jenkins cp's docker-compose-and-run to aws server
-
+  scp -o StrictHostKeyChecking=no -i "~/workspace/TicTacToe_Deploy/KarlUser-key-pair-eu-west-1.pem" ./build/.env ec2-user@52.208.180.163:~/.env
 exit 0
