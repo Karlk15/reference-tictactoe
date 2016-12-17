bc360e3e2832155cce3584dd0b72ebeda202154f
Karl K <Karlk15@ru.is>
2016-12-17 20:04:04 +0000

    script for jenkins to run apitest and setup npm

---
+++ scripts/jenkinsApitest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git clean -dfx        # Remove untracked files from the working tree...
+git stash             # Stash the changes in a dirty working directory away
+rm -rf node_modules   # Remove node_modules from root
+npm cache clean
+npm install           # installs npm
+cd client             # enters client
+rm -rf node_modules   # Remove node_modules from client
+npm cache clean
+npm install           # installs npm should only be localy
+cd ..
+npm run apitest
