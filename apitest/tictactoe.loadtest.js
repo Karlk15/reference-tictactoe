5cf98a632e7c159c9156dd08c6c698949bc7d575
Karl K <Karlk15@ru.is>
2016-12-19 00:39:32 +0000

    time extend

--- apitest/tictactoe.loadtest.js
+++ apitest/tictactoe.loadtest.js
@@ -13,7 +13,7 @@ const testAPI = TestAPI(inject({
     RoutingContext
 }));
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
 
 describe('Tictactoe load test', function(){
 
@@ -27,7 +27,7 @@ describe('Tictactoe load test', function(){
     });
 
     const count = 100;
-    const timelimit = 50000;
+    const timelimit = 80000;
 
     it('should be able to play game to end ' + count + ' times within '+ timelimit +'ms',function(done){
 
